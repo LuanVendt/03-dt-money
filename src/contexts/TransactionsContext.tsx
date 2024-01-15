b87096dd891ef86f3230c90ebf43c0837b599444
Luan Vendt <luanvendt@gmail.com>
2024-01-15 15:20:14 -0300

    Aplicando o memo

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -60,7 +60,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       setTransactions((state) => [response.data, ...state])
     },
     [],
-  )
+  ) 
 
   useEffect(() => {
     fetchTransactions()
